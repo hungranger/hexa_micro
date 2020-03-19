c32c1221c735486b2f8c352f1782c2a779545a71
hung vu <hung.vu@mobytelab.com>
2020-03-20 00:50:39 +0700

    implement main function work with environment variables

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	h "hexa_micro/api"
+	mongo "hexa_micro/repository/mongodb"
+	"hexa_micro/repository/redis"
+	shortener "hexa_micro/shotener"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	repo := chooseRepo()
+	service := shortener.NewRedirectService(repo)
+	handler := h.NewHandler(service)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{code}", handler.Get)
+	r.Post("/", handler.Post)
+
+	errs := make(chan error, 2)
+	go func() {
+		fmt.Println("Listening on port :8000")
+		errs <- http.ListenAndServe(httpPort(), r)
+
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	fmt.Printf("Terminated %s", <-errs)
+}
+
+func httpPort() string {
+	port := "8080"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	return fmt.Sprintf(":%s", port)
+}
+
+func chooseRepo() shortener.RedirectRepository {
+	switch os.Getenv("URL_DB") {
+	case "redis":
+		redisURL := os.Getenv("REDIS_URL")
+		repo, err := redis.NewRedisRepository(redisURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	case "mongo":
+		mongoURL := os.Getenv("MONGO_URL")
+		mongodb := os.Getenv("MONGO_DB")
+		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
+		repo, err := mongo.NewMongoRepository(mongoURL, mongodb, mongoTimeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	}
+	return nil
+}
